2abb5af899b48e5229a2c03810e3e6625b66c0a4
MarvinKnobloch <NivramKnobloch@gmail.com>
2024-12-22 15:33:42 +0100

    Scene Stuff

--- Assets/Scripts/UI/IntroScene.cs
+++ Assets/Scripts/UI/IntroScene.cs
@@ -1,10 +1,11 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Santa;
 
 public class IntroScene : MonoBehaviour
 {
     public void StartGame()
     {
-        SceneManager.LoadScene(2);
+        SceneManager.LoadScene((int)SceneEnum.Level1);
     }
 }
